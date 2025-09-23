d6c173d8bd1a975bd0f13a02b9676220444d626e
tavo1599 <gustavo08_99@hotmail.com>
2025-09-23 18:26:36 -0500

    produccion correccion

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  // --- ESTA ES LA LÍNEA CORREGIDA ---
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Interceptor para añadir el token a cada petición
+// Interceptor (sin cambios)
 apiClient.interceptors.request.use(
   (config) => {
-    // Obtenemos el store DENTRO del interceptor para asegurar que esté inicializado
     const authStore = useAuthStore();
     const token = authStore.token;
     
